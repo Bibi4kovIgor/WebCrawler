05d12f1f470d9aff6e0492b4d6b30b03c40e497b
Bibi4kovIgor <work.bibi4kov@gmail.com>
2018-04-20 16:13:07 +0300

    First release

---
+++ src/main/java/main.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class main {
+    public static void Main(String[] args) throws IOException {
+        System.out.println("Input Wiki URL");
+        BufferedReader br = new BufferedReader((new InputStreamReader(System.in)));
+        String inputURL = br.readLine();
+
+
+
+    }
+}
